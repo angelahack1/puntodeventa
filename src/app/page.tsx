58d6c6c0f0a6e7c02a15e2056c1575db4b98313f
angelahack1 <angelakimichellle@hotmail.com>
2025-07-07 01:37:16 -0600

    getting back to static, by angelahack1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 import ProductCard from "@/components/ProductCard";
 import { getProductsFromDB } from "@/db/db_access";
 
-export const dynamic = 'force-dynamic';
-
 export default async function Home() {
   const products = await getProductsFromDB();
   return (
